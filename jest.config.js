4464621fc768d7e19fd31cc87792a4318a5f5ef6
tangjiahao <tangjiahao@shein.com>
2019-10-29 14:22:01 +0800

    看源码时通过单测调试的小办法

--- jest.config.js
+++ jest.config.js
@@ -23,5 +23,6 @@ module.exports = {
     '^@vue/(.*?)$': '<rootDir>/packages/$1/src'
   },
   rootDir: __dirname,
+  // 可以修改下面这里的匹配字符串, 在看源码时每次只调试你在看的部分的测试代码
   testMatch: ['<rootDir>/packages/**/__tests__/**/*spec.[jt]s?(x)']
 }
